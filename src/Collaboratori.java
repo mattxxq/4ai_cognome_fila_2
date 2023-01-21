5def70a335646b7712f92f1e26dbc8c622b8e24c
mattxxq <122672870+mattxxq@users.noreply.github.com>
2023-01-21 11:56:49 +0100

    Update Collaboratori.java

--- src/Collaboratori.java
+++ src/Collaboratori.java
@@ -4,11 +4,7 @@ public class Collaboratori {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Filoni Matteo");
-		System.out.println("Molle Samuele");
-		System.out.println("Quintana Simone");
-                System.out.println("Portaccio Giacomo");
-		System.out.println("Filoni Gioele");
-		System.out.println("Rainò Michael");
+		
 	}
 
 }
