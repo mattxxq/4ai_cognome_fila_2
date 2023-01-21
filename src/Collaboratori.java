50df6aaa6b46fdd33ff72667fa1326e512662fe1
4a <4a@pianodue06>
2023-01-21 12:10:00 +0100

    aggiunta nome portaccio giacomo

--- src/Collaboratori.java
+++ src/Collaboratori.java
@@ -5,6 +5,7 @@ public class Collaboratori {
 		// TODO Auto-generated method stub
 		System.out.println("Filoni Matteo");
 		System.out.println("Filoni Gioele");
+		System.out.println("Portaccio Giacomo");
 		
 	}
 
