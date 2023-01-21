3abd89c65c85dd5d97b6f829e57064b0a68494b0
MolleSamuele <122672881+MolleSamuele@users.noreply.github.com>
2023-01-21 11:51:34 +0100

    Update Collaboratori.java

--- src/Collaboratori.java
+++ src/Collaboratori.java
@@ -4,8 +4,8 @@ public class Collaboratori {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Filoni Matteo");
-		System.out.println("Molle Samuelo");
-		System.out.println("Quintana Simono");
+		System.out.println("Molle Samuele");
+		System.out.println("Quintana Simone");
                 System.out.println("Portaccio Giacomo");
 		System.out.println("Filoni Gioele");
 		System.out.println("Rainò Michael");
