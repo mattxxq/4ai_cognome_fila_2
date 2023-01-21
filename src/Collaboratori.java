82a24f7a888173a56fe6fb224170198aee77db83
GiacomoPr <122673125+GiacomoPr@users.noreply.github.com>
2023-01-21 11:50:41 +0100

    Update Collaboratori.java

--- src/Collaboratori.java
+++ src/Collaboratori.java
@@ -4,8 +4,8 @@ public class Collaboratori {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Filoni Matteo");
-		System.out.println("Molle Samuela");
-		System.out.println("Quintana Simona");
+		System.out.println("Molle Samuelo");
+		System.out.println("Quintana Simono");
                 System.out.println("Portaccio Giacomo");
 		System.out.println("Filoni Gioele");
 		System.out.println("Rainò Michael");
