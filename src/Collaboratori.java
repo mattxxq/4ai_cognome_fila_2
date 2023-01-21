09697283ff2fc21ea8f8f8555942377a4d161782
4a <4a@pianodue09>
2023-01-21 12:07:15 +0100

    aggiunta nome Gioele Filoni

--- src/Collaboratori.java
+++ src/Collaboratori.java
@@ -4,6 +4,7 @@ public class Collaboratori {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Filoni Matteo");
+		System.out.println("Filoni Gioele");
 		
 	}
 
